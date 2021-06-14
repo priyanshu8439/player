76695005146e7bb1190a36b1dfaca3579875f4d3
priyanshu8439 <bansal8439@gmail.com>
2021-06-14 14:51:48 +0530

    First commit

---
+++ Controllers/PlayersController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class PlayersController1 : Controller
+    {
+        static List<Player> list = new List<Player>()
+        {
+            new Player{PId=1,PName="Rohit",PTeam="India",PCategory="All-Rounder"},
+            new Player{PId=2,PName="Virat",PTeam="CSK",PCategory="Batsman"},
+            new Player{PId=3,PName="Dhoni",PTeam="India",PCategory="Bowler"},
+            new Player{PId=4,PName="Mohd Shami",PTeam="Banglore",PCategory="Wicket-Keeper"},
+        };
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+    }
+}
